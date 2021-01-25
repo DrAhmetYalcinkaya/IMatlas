fb8bebb6e38e7a9a661809074276f5e2c6bc365b
Pascal <p.maas92@gmail.com>
2021-01-25 16:39:36 +0100

    Update of DESCRIPTION
    Fixed bug when installing GO.db and AnnotationDBI from Bioconductor
    Added some documentation and a license

---
+++ build.R
@@ -0,0 +1,32 @@
+library(devtools)
+library(roxygen2)
+library(usethis)
+setwd("~/ImmunoMet/ImmunoMet")
+usethis::use_description()
+usethis::use_mit_license("Pascal Maas")
+packages <- c("shiny", "igraph", "colourpicker", "httr", "dplyr", 
+              "shinythemes", "shinycssloaders", "waiter", "AnnotationDbi",
+              "shinydashboard", "htmlwidgets", "heatmaply", "DT", "yaml",
+              "RColorBrewer", "shinyalert", "plotly", "shinyjs", "data.table", 
+              "formattable", "gtools", "ggplot2", "GO.db", "plyr")
+invisible(sapply(packages, usethis::use_package))
+
+
+setwd("~/ImmunoMet/ImmunoMet")
+devtools::load_all()
+devtools::document()
+rcmdcheck::rcmdcheck()
+devtools::install(upgrade = "never")
+
+
+devtools::install_github(repo = "vanhasseltlab/ImmuneMetAtlas", 
+                         auth_token = "745f3796c07ee277ce332f2c155ff955a64093aa")
+
+
+library(ImmunoMet)
+load_data("C:/Users/Pascal/Documents/ImmuneMetAtlas/App/config.yaml")
+run_shiny()
+plot(example_graph())
+to_plotly(example_graph())
+
+
