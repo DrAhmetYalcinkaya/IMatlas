e79ee9d67d321bea0a042201e9af3ec8bc8a2324
Pascal <p.maas92@gmail.com>
2021-05-31 09:45:07 +0200

    Added port option for running shiny

--- R/app.R
+++ R/app.R
@@ -1,8 +1,10 @@
 #'@title Run Shiny App
 #'@usage run_shiny(
-#'    browser = TRUE
+#'    browser = TRUE,
+#'    port = 2222
 #')
 #'@param browser Should the browser be started?
+#'@param port Which port to be used
 #'@examples
 #'# Start the Atlas in your browser
 #'\dontrun{
@@ -10,11 +12,11 @@
 #'}
 #'@rawNamespace import(shiny, except = c(dataTableOutput, renderDataTable))
 #'@export
-run_shiny <- function(browser = TRUE){
+run_shiny <- function(browser = TRUE, port = 2222){
   env <- sys.frame()
   if (is.null(env$interactions)) stop("No data loaded. Run 'load_data(config_path)' first.", call. = F)
   app <- shinyApp(ui, server)
-  runApp(app, launch.browser = browser)
+  runApp(app, launch.browser = browser, port = port)
 }
 
 
